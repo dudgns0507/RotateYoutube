8c513a7095e4bfbec84f6f18bece8a8421cea6e2
Park Young-Hoon <hoon2585@gmail.com>
2019-09-18 02:29:14 +0900

    Update
    Change pixels to percentage

--- popup.js
+++ popup.js
@@ -11,12 +11,12 @@ $(function() {
                 chrome.tabs.executeScript(tabs[0].id,{code:`setInterval(function() {
                     if(1 >= window.outerHeight - window.innerHeight) {
                         document.querySelectorAll('.html5-video-player')[0].style.top = "0px";
-                        document.querySelectorAll('.html5-main-video')[0].style.left = "400px";
+                        document.querySelectorAll('.html5-main-video')[0].style.left = "21%";
                         document.querySelectorAll('.html5-main-video')[0].style.width = "100vh";
                     } else {
-                        document.querySelectorAll('.html5-video-player')[0].style.top = "300px";
-                        document.querySelectorAll('.html5-video-player')[0].style.left = "-200px";
-                        document.querySelectorAll('.html5-main-video')[0].style.width = "calc(100vh - 80px)";
+                        document.querySelectorAll('.html5-video-player')[0].style.top = "40%";
+                        document.querySelectorAll('.html5-video-player')[0].style.left = "-18%";
+                        document.querySelectorAll('.html5-main-video')[0].style.width = "calc(100vh - 4%)";
                     }
                 }, 100)`});
             });
