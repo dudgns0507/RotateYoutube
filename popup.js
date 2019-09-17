667f14640f2e6ce3dce60c06fc9e6bb147e1b6f4
Park Young-Hoon <hoon2585@gmail.com>
2019-09-18 01:57:46 +0900

    Update
    Detect fullscreen to change video size

--- popup.js
+++ popup.js
@@ -5,13 +5,20 @@ $(function() {
             chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT, active: true}, function(tabs) {
                 chrome.tabs.executeScript(tabs[0].id,{code:`document.querySelectorAll('.html5-video-player')[0].style.transform = "rotate(90deg)";`});
                 chrome.tabs.executeScript(tabs[0].id,{code:`document.querySelectorAll('.html5-video-player')[0].style.margin = "0 auto";`});
-                chrome.tabs.executeScript(tabs[0].id,{code:`document.querySelectorAll('.html5-video-player')[0].style.width = "100vh";`});
 
                 chrome.tabs.executeScript(tabs[0].id,{code:`document.querySelectorAll('.ytp-fullscreen-button')[0].click();`});
 
-                chrome.tabs.executeScript(tabs[0].id,{code:`setTimeout(function() {
-                    document.querySelectorAll('.html5-main-video')[0].style.width = "100vh";
-                }, 300)`});
+                chrome.tabs.executeScript(tabs[0].id,{code:`setInterval(function() {
+                    if(1 >= window.outerHeight - window.innerHeight) {
+                        document.querySelectorAll('.html5-video-player')[0].style.top = "0px";
+                        document.querySelectorAll('.html5-main-video')[0].style.left = "400px";
+                        document.querySelectorAll('.html5-main-video')[0].style.width = "100vh";
+                    } else {
+                        document.querySelectorAll('.html5-video-player')[0].style.top = "300px";
+                        document.querySelectorAll('.html5-video-player')[0].style.left = "-200px";
+                        document.querySelectorAll('.html5-main-video')[0].style.width = "calc(100vh - 80px)";
+                    }
+                }, 100)`});
             });
         });
     });    
